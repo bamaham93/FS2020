dc19d2ccec9842e2b2bba5e559821d3e68ccce89
bamaham93 <bamaham93@gmail.com>
2022-04-15 20:18:27 -0400

    Configured static folders, tested static assets.

--- python_anywhere_website/python_anywhere_website/settings.py
+++ python_anywhere_website/python_anywhere_website/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "python_anywhere_website/static",
+    '/var/www/static/',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
