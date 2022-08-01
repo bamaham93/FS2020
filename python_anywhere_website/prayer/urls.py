e45c0964b1d9af510b3d02b729489a928bcd2e62
bamaham93 <bamaham93@gmail.com>
2022-08-01 17:47:54 -0400

    Added pages to prayer app.

--- python_anywhere_website/prayer/urls.py
+++ python_anywhere_website/prayer/urls.py
@@ -7,4 +7,7 @@ app_name = "prayer"
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
+    path('new-message', views.new_message, name='new_message'),
+    path('groups', views.groups, name='groups'),
+    path('prayer-requests', views.prayer_requests, name='prayer_requests'),
 ]
--- python_anywhere_website/prayer/views.py
+++ python_anywhere_website/prayer/views.py
@@ -6,3 +6,24 @@ def index(request):
     """
     context = {}
     return render(request, 'prayer/index.html', context)
+
+
+def new_message(request):
+    """
+    """
+    context = {}
+    return render(request, 'prayer/new_message.html', context)
+
+
+def groups(request):
+    """
+    """
+    context = {}
+    return render(request, 'prayer/groups.html', context)
+
+def prayer_requests(request):
+    """
+
+    """
+    context = {}
+    return render(request, 'prayer/prayer_request.html', context)
