dc62fb01357e8831c2fbf611f4dfed8c3790e16e
bamaham93 <bamaham93@gmail.com>
2022-08-08 18:04:36 -0400

    Added pages for each media type.

--- python_anywhere_website/media/urls.py
+++ python_anywhere_website/media/urls.py
@@ -6,4 +6,12 @@ app_name = "media"
 urlpatterns = [
     path("", views.index, name="index"),
     path("index", views.index, name="index"),
+    path("movies", views.movies, name="movies"),
+    path("books", views.books, name="books"),
+    path('cds', views.cds, name='cds'),
+    path('dvds', views.dvds, name='dvds'),
+    path('amazon', views.amazon, name='amazon'),
+    path('youtube', views.youtube, name='youtube'),
+    path('digital-dl', views.digital_dl, name="digital_dl"),
+    path('vhs', views.vhs, name='vhs'),
 ]
