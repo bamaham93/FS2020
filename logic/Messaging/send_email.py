0688a87570a27a7f54fc838cd47f1ac7e05be74e
bamaham93 <bamaham93@gmail.com>
2022-09-01 02:35:35 -0400

    Added send_email.py contents, configured to use API call.

--- logic/Messaging/send_email.py
+++ logic/Messaging/send_email.py
@@ -1,5 +1,34 @@
 """
-Sends the email messages.
+Sends email messages using SendGrid's Python Library
+https://github.com/sendgrid/sendgrid-python
 """
 
 import logging
+import os
+
+from datetime.datetime import now
+from dotenv import load_env
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_env()
+
+
+def _send_email(to_address: str, subject: str, body: str):
+    """
+    Note that body can be HTML stlyed.
+    """
+    message = Mail(
+        from_email='info@jacob-mcgowin.us',
+        to_emails=f'{to_address}',
+        subject=f'{subject}',
+        html_content=f'{body}')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        # print(e.message)
+        logging.warning(f"{now} {e}")
