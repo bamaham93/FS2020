4cd476a79e2e2d0127d29067bc2cb810c6471c71
bamaham93 <bamaham93@gmail.com>
2022-09-01 02:26:15 -0400

    Adding .env to project for secrets and configuration.

--- python_anywhere_website/python_anywhere_website/settings.py
+++ python_anywhere_website/python_anywhere_website/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_env
+
+load_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
