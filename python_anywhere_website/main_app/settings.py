15ce978e81e3552c8cbc923313100802aaf18a40
bamaham93 <bamaham93@gmail.com>
2022-11-18 15:14:34 -0500

    Config changes to fix prod.

--- python_anywhere_website/main_app/settings.py
+++ python_anywhere_website/main_app/settings.py
@@ -131,11 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = "/home/Bamaham93/FS2020/python_anywhere_website/static"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/var/www/static/",
+    # "/var/www/static/",
 ]
 
 # Default primary key field type
