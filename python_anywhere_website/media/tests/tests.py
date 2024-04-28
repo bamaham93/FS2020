80009a67d9c556472bf30e9002c1a05194ecd60e
Jacob McGowin <bamaham93@gmail.com>
2024-04-27 20:39:14 -0400

    Adding the start of testing to the Media app.

---
+++ python_anywhere_website/media/tests/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestMyModel(TestCase):
+    """
+    """
+
+    def setUp(self):
+        """
+        """
+
+    def test_thing1(self):
+        """
+        """
+        self.assertTrue(True)
+
+    def test_render_template(self):
+        """
+        """
+        self.assertTrue(True)
